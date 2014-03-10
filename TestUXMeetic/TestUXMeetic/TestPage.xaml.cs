a70cc724e3e9b8ae3a2264fb63caf6d2f10d9116
tsalandre <t.salandre@meetic-corp.com>
2014-03-10 13:54:50 +0100

    testPageCommit
    test Page commit

---
+++ TestUXMeetic/TestUXMeetic/TestPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace TestUXMeetic
+{
+    public partial class TestPage : PhoneApplicationPage
+    {
+        public TestPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
